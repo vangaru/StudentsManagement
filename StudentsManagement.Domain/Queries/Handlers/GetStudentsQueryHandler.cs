72910ab491b5a6dbe7dbffda9667b413a3d9438c
i.ivanenko <i.ivanenko@itransition.com>
2023-01-10 22:47:55 +0300

    forgot in latest commit

---
+++ StudentsManagement.Domain/Queries/Handlers/GetStudentsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using StudentsManagement.Domain.Models;
+using StudentsManagement.Domain.Repositories;
+
+namespace StudentsManagement.Domain.Queries.Handlers;
+
+public class GetStudentsQueryHandler : IRequestHandler<GetStudentsQuery, IEnumerable<Student>>
+{
+    private readonly IRepository<Student> _studentsRepository;
+
+    public GetStudentsQueryHandler(IRepository<Student> studentsRepository)
+    {
+        _studentsRepository = studentsRepository;
+    }
+
+    public Task<IEnumerable<Student>> Handle(GetStudentsQuery request, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return _studentsRepository.GetEntitiesAsync();
+    }
+}
